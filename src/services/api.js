c394153dce1d37fdaeea7dd178636a659085be16
thyago henrique pacher <thyago.pacher@gmail.com>
2021-06-14 07:49:06 -0300

    arrumando axios

--- src/services/api.js
+++ src/services/api.js
@@ -8,9 +8,22 @@ const urlApi = 'https://hackathon-api.andreazza.dev';
 
 const api = axios.create({
     baseURL: urlApi,
+    withCredentials: false,
     headers: {
-        "Content-type": "application/json"
+        "Content-type": "application/json;charset=utf-8",
     }
+});
+
+api.interceptors.response.use((response) => response, (error) => {
+    let msgErro = error;
+    if (error.response.status == 405) {
+        msgErro = 'Método HTTP não implementado no servidor !';
+    } else if (error.response.status == 404) {
+        msgErro = 'Não encontrado';
+    } else if (error.response.status == 500) {
+        msgErro = 'Falha no servidor';
+    }
+    throw msgErro;
 });
 
 export default api;
